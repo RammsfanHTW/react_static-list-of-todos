3cc45d9b94a9f8f1f07e9da89f9a914b1e3f9e5b
RammsfanHTW <64034634+RammsfanHTW@users.noreply.github.com>
2020-06-30 08:30:38 +0300

    Updates

---
+++ precache-manifest.358a97611acfae75ed4f1995ca3ab330.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62c4894ee6d435ecd8f6c52eb079a961",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c09ac1107c7e37c359a1",
+    "url": "./static/css/main.45b3208b.chunk.css"
+  },
+  {
+    "revision": "5b54a948430fb7726cd3",
+    "url": "./static/js/2.af8e5a81.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.af8e5a81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c09ac1107c7e37c359a1",
+    "url": "./static/js/main.c915262e.chunk.js"
+  },
+  {
+    "revision": "ec438ca3dae444460a4d",
+    "url": "./static/js/runtime-main.1d48480d.js"
+  }
+]);
